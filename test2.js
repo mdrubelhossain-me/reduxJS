9665daead9441c476039eba7fdce5918e54c9661
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-05-05 00:20:56 +0600

    user added

---
+++ test2.js
@@ -0,0 +1,42 @@
+// Initial state
+const initialState = {
+    users: ["Doe"],
+    count: 1,
+  };
+  
+  // Action type
+  const ADD_USER = "ADD_USER";
+  
+  // Action creator
+  const addUser = (user) => {
+    return {
+      type: ADD_USER,
+      payload: user,
+    };
+  };
+  
+  // Reducer
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case ADD_USER:
+        return {
+          ...state,
+          users: [...state.users, action.payload],
+        };
+      default:
+        return state;
+    }
+  };
+  
+  // Create store
+  const { createStore } = require("redux");
+  const store = createStore(reducer);
+  
+  // Subscribe to store
+  store.subscribe(() => {
+    console.log("State updated:", store.getState());
+  });
+  
+  // Dispatch action
+  store.dispatch(addUser("John")); // State updated: { users: [ 'Doe', 'John' ], count: 1 }
+  
