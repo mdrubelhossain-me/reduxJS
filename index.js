4b8c4288f88e581e11b20aa4b72d08139e903d76
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-05-04 16:27:39 +0600

    store created

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //state - count 0
 //action - increment, decrement, reset
 //reducer - function that takes state and action and returns new state
@@ -12,9 +11,51 @@ const initialState = {
 
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
+const RESET = "RESET";
 
+//initial state
 const incrementAction = () => {
   return {
     type: INCREMENT,
   };
-}
+};
+
+//decrement action
+const decrementAction = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+//reset action
+const resetAction = () => {
+  return {
+    type: RESET,
+  };
+};
+
+
+//reducer function
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case RESET:
+      return { ...state, count: 0 };
+    default:
+      return state;
+  }
+};
+
+//create store
+const { createStore } = require("redux");
+const store = createStore(counterReducer);
+//subscribe to store
+store.subscribe(() => {
+  console.log("State changed:", store.getState());
+});
+//dispatch actions
+store.dispatch(incrementAction()); // count: 1
+store.dispatch(incrementAction()); // count: 2
