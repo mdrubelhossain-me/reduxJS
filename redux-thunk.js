47b8a5438dfd5e2a9022b457263cafaa43840603
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-05-05 23:36:50 +0600

    redux-thunk

---
+++ redux-thunk.js
@@ -0,0 +1,27 @@
+//async action- api call
+//api url: https://jsonplaceholder.typicode.com/todos
+//redux-thunk is a middleware that allows you to write action creators that return a function instead of an action.
+//axios is a promise-based HTTP client for the browser and Node.js.
+
+
+//constants
+
+
+
+//state
+const initialState = {
+  todos: [],
+  loading: false,
+  error: null,
+};
+
+
+//action
+
+
+
+//reducer
+
+
+
+//store
